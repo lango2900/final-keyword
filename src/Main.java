179d37397d7e69ea515ea231d55e17a7ff25e690
lango <mlangone221@hcc.edu>
2023-04-16 22:13:17 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // final keyword - anything declared this way cannot be changed or updated in the final program
+
+        final double pi = 3.14159; // other instance of pi invalid with 'final' keyword
+
+        //  pi = 4;
+
+        System.out.println(pi);
+
+    }
+}
